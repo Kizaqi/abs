78c251056af1df7b35730098868f17d7d79df8a1
Mykhailo Luzhetskyi <luzeckijmihajlo0@gmail.com>
2023-02-20 19:32:03 +0200

    commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,40 +1,58 @@
 function burgerMenu(selector) {
-    let menu = $(selector);
-    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
-    let links = menu.find('.burger-menu_link');
-    let overlay = menu.find('.burger-menu_overlay');
-    
-    button.on('click', (e) => {
-      e.preventDefault();
-      toggleMenu();
-    });
-    
-    links.on('click', () => toggleMenu());
-    overlay.on('click', () => toggleMenu());
-    
-    function toggleMenu(){
-      menu.toggleClass('burger-menu_active');
-      
-      if (menu.hasClass('burger-menu_active')) {
-        $('body').css('overlow', 'hidden');
-      } else {
-        $('body').css('overlow', 'visible');
-      }
+  let menu = $(selector);
+  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  let links = menu.find('.burger-menu_link');
+  let overlay = menu.find('.burger-menu_overlay');
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass('burger-menu_active');
+
+    if (menu.hasClass('burger-menu_active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
     }
   }
+}
+$('.owl-carousel').owlCarousel({
+  stagePadding: 260,
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: true,
+    },
+  },
+});
 
+var owl = $('.owl-carousel');
+owl.owlCarousel();
 
-// var owl = $('.owl-carousel');
-// owl.owlCarousel();
-// Go to the next item
-// $('.next').click(function() {
-//     owl.trigger('next.owl.carousel');
-// })
+$('.next').click(function () {
+  owl.trigger('next.owl.carousel');
+});
 
-// $('.previous').click(function() {
-//     owl.trigger('prev.owl.carousel', [300]);
-// })
+$('.previous').click(function () {
+  owl.trigger('prev.owl.carousel');
+});
 
-  
 burgerMenu('.burger-menu');
-  
